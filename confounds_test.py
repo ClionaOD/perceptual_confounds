5319d5d399658c0ae4ab836b02f6c91584705026
ClionaOD <odoherc1@tcd.ie>
2020-11-17 13:48:07 +0000

    first test

---
+++ confounds_test.py
@@ -0,0 +1,40 @@
+import skvideo.io
+import numpy as np
+from skimage import color
+
+class RGB2Lab(object):
+    """Convert RGB PIL image to ndarray Lab."""
+    def __call__(self, img):
+        img = np.asarray(img, np.uint8)
+        img = color.rgb2lab(img)
+        return img
+
+def load_video(infn, clipheight=None):
+    # Load metadata
+    metadata = skvideo.io.ffprobe(infn)
+    dur=float(metadata['video']['@duration'])       
+    nframes=int(float(metadata['video']['@nb_frames']))
+    fps=nframes/dur
+    print('Video %s duration %f nframes %d fps %f'%(infn,dur,nframes,fps))
+
+    # Load a video
+    singlevideo=skvideo.io.vread(infn)
+    print(singlevideo.shape)
+    singlevideo=singlevideo.astype(np.uint8)
+
+    # Clip height?
+    if clipheight:
+        h=singlevideo.shape[1]
+        singlevideo=singlevideo[:,round(h/2-clipheight/2):round(h/2+clipheight/2),:,:]
+        metadata['video']['@height']=clipheight
+        print('Clipped height to %s'%clipheight)
+    
+    return metadata,singlevideo,dur,fps
+
+vid = './bathsong.mp4'
+metadata, singlevideo, dur, fps= load_video(vid)
+
+img = singlevideo[0,:,:,:]
+
+lab_img = RGB2Lab(img)
+print('Image converted')
