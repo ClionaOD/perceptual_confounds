f362c07e181e7336657b1c3371ad54c93dbbcf93
ClionaOD <odoherc1@tcd.ie>
2020-11-23 11:29:40 +0000

    save as csv not pickle

--- confounds_test.py
+++ confounds_test.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 import pandas as pd
 import skvideo.io
 import numpy as np
@@ -85,9 +84,10 @@ def get_gcf(vidPath):
             all_gcfs[idx] = compute_global_contrast_factor(frame)
         
         framewise_gcf[vid] = all_gcfs
-        mean_gcf[vid] = [np.mean(all_gcfs)]
+        mean = np.mean(all_gcfs)
+        mean_gcf[vid] = [mean]
 
-        print(f'{vid} mean gcf = {mean_gcf}')
+        print(f'{vid} mean gcf = {mean}')
 
     framewise_gcf = pd.DataFrame.from_dict(framewise_gcf)
     mean_gcf = pd.DataFrame.from_dict(mean_gcf)
@@ -100,11 +100,9 @@ if __name__ == "__main__":
     movie_times = pd.read_csv('./test_movie.csv',sep=';', index_col='title')
     
     #crop all videos in vidPath according to start/end in movie_times
-    #crop_movies(vidPath, movie_times)
+    crop_movies(vidPath, movie_times)
 
     #calculate global contrast function and save the dataframes
     framewise_gcf, mean_gcf = get_gcf(vidPath)
-    with open('./framewise_gcf.pickle','wb') as f:
-        pickle.dump(framewise_gcf, f)
-    with open('./mean_gcf.pickle','wb') as f:
-        pickle.dump(mean_gcf, f)
+    framewise_gcf.to_csv('./framewise_gcf.csv',index=False)
+    mean_gcf.to_csv('./mean_gcf.csv',index=False)
