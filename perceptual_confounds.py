3d319056e9130488de54f312752ce1c4da45c553
ClionaOD <odoherc1@tcd.ie>
2020-11-27 14:05:41 +0000

    test for framerate change

--- perceptual_confounds.py
+++ perceptual_confounds.py
@@ -32,15 +32,26 @@ def load_video(infn, clipheight=None):
 def runBash(command):
     os.system(command)
 
-def crop(start,end,inPath,outPath):
+def crop(start,dur,inPath,outPath):
     """
     args:
-    start: start time for trimmed video in form hh:mm:ss
-    end: end time for trimmed video in form hh:mm:ss
-    inPath: path to video to trim, include extension '*.mp4'
-    outPath: path to save trimmed video, include extension '*.mp4'
+        start: start time for trimmed video in form hh:mm:ss
+        dur: the length of desired clip in form hh:mm:ss. e.g. 23 s from start is 00:00:23
+        inPath: path to video to trim, include extension '*.mp4'
+        outPath: path to save trimmed video, include extension '*.mp4'
     """
-    ffmpegCommand = "ffmpeg -i " + inPath + " -ss  " + start + " -to " + end + " -c copy " + outPath
+    ffmpegCommand = f"ffmpeg -i {inPath} -ss {start} -t {dur} {outPath}"
+    print(ffmpegCommand)
+    runBash(ffmpegCommand)
+
+def change_framerate(fps,inPath,outPath):
+    """
+    args:
+        fps: desired framerate
+        inPath: path to video to change fps, include extension '*.mp4'
+        outPath: path to save video, must be separate to inPath, include extension '*.mp4'
+    """
+    ffmpegCommand = f"ffmpeg -i {inPath} -filter:v fps=fps={fps} {outPath}"
     print(ffmpegCommand)
     runBash(ffmpegCommand)
 
@@ -52,20 +63,44 @@ def crop_movies(vid_path, movie_times):
         vid_path: the path to find videos to trim
         movie_times: pandas dataframe containing cols:[vid_title, start_time, end_time]
     """
+    dur = '00:00:22.500'
     for vid in os.listdir(vidPath):
         if not os.path.isdir(f'{vidPath}/{vid}'):
+            
             #crop the video using ffmpeg command line
             start = movie_times.loc[vid,'start']
-            end = movie_times.loc[vid,'end']
-            inPath = os.path.join(vidPath,vid)
+            inPath = f'{vidPath}/{vid}'
             outPath = f'{vidPath}/trimmed/{vid}'
 
-            crop(start,end,inPath,outPath)
+            crop(start,dur,inPath,outPath)
             print(f'{vid} trimmed')
 
-def get_gcf(vidPath):
-    #TODO: make gcf a separate function
-    #TODO: make this a get_confounds function including rms
+            #set to mean frame rate
+            fps = pd.read_csv('./fps.csv')
+            change_framerate(np.mean(fps.values), f'{vidPath}/trimmed/{vid}', f'{vidPath}/fps/{vid}')
+            print(f'{vid} frame rate standardised')
+
+def get_gcf(vid, frame_dict, mean_dict):
+    """
+    args:
+        vid: the video get contrast of
+        frame_df: dictionary to store framewise gcf values with k=vid and v=list
+        mean_df: dictionary to store mean gcf values with k=vid and v=[mean]
+    """
+    
+    all_gcfs = np.zeros(vid.shape[0])
+    for idx, frame in enumerate(vid):
+        all_gcfs[idx] = compute_global_contrast_factor(frame)
+        
+    frame_dict[vid] = all_gcfs
+    mean = np.mean(all_gcfs)
+    mean_dict[vid] = [mean]
+
+    print(f'{vid} mean gcf = {mean}')
+
+
+def get_confounds(vidPath):
+    #TODO: include rms
     """
     calculate gcf using code from Matkovic et al for each video
     args:  
@@ -74,24 +109,16 @@ def get_gcf(vidPath):
         framewise_gcf: a pandas dataframe with indices=movie_titles and values=gcf for each frame
         mean_gcf: the mean gcf for each video
     """
-    
-    framewise_gcf = {k:[] for k in os.listdir(f'{vidPath}/trimmed')}
-    mean_gcf = {k:[] for k in os.listdir(f'{vidPath}/trimmed')}
+
+    framewise_gcf = {k:[] for k in os.listdir(f'{vidPath}')}
+    mean_gcf = {k:[] for k in os.listdir(f'{vidPath}')}
     
     #load the cropped video
-    for vid in os.listdir(f'{vidPath}/trimmed'):
-        metadata, singlevideo, dur, fps = load_video(f'{vidPath}/trimmed/{vid}')
+    for vid in os.listdir(f'{vidPath}'):
+        metadata, singlevideo, dur, fps = load_video(f'{vidPath}/{vid}')
         print(f'{vid} loaded')
         
-        all_gcfs = np.zeros(singlevideo.shape[0])
-        for idx, frame in enumerate(singlevideo):
-            all_gcfs[idx] = compute_global_contrast_factor(frame)
-        
-        framewise_gcf[vid] = all_gcfs
-        mean = np.mean(all_gcfs)
-        mean_gcf[vid] = [mean]
-
-        print(f'{vid} mean gcf = {mean}')
+        get_gcf(vid, framewise_gcf, mean_gcf)
 
     framewise_gcf = pd.DataFrame.from_dict(framewise_gcf, orient='index')
     mean_gcf = pd.DataFrame.from_dict(mean_gcf, orient='index')
@@ -104,21 +131,19 @@ def get_gcf(vidPath):
 if __name__ == "__main__":
     
     vidPath = '/home/clionaodoherty/foundcog_stimuli'
-    """movie_times = pd.read_csv('./movie_times.csv',sep=';', index_col='title')
+    movie_times = pd.read_csv('./movie_times.csv',sep=';', index_col='title')
     
     #crop all videos in vidPath according to start/end in movie_times
-    #TODO:make movie crops same length (22.5 s)
-    #TODO:make movie crops same frame rate
     crop_movies(vidPath, movie_times)
 
     #calculate global contrast function and save the dataframes
-    framewise_gcf, mean_gcf = get_gcf(vidPath)
+    framewise_gcf, mean_gcf = get_confounds(f'{vidPath}/fps')
     framewise_gcf.to_csv('./framewise_gcf.csv')
-    mean_gcf.to_csv('./mean_gcf.csv')"""
+    mean_gcf.to_csv('./mean_gcf.csv')
 
     df = pd.DataFrame(index=os.listdir(vidPath),columns=['fps'])
     for vid in os.listdir(vidPath):
         if not os.path.isdir(f'{vidPath}/{vid}'):
             metadata, singlevideo, dur, fps = load_video(f'{vidPath}/trimmed/{vid}')
             df.loc[vid]=fps
-    df.to_csv('./fps.csv')
+    df.to_csv('./fps_redo.csv')
