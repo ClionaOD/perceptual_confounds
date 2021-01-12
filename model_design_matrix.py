a34eafb7b8b87c73bf338d4617285fc58bed06f0
hanmacrad2 <craddoch@tcd.ie>
2021-01-12 22:38:53 +0000

    efficiency - attempt #1
    efficiency - attempt #1

--- model_design_matrix.py
+++ model_design_matrix.py
@@ -12,6 +12,8 @@ tr = 1.0
 n_scans = 22
 frame_times = np.arange(n_scans) * tr
 
+%matplotlib qt
+
 all_design_matrices = {k:None for k in all_events.keys()}
 for vid, events in all_events.items():
     X = make_first_level_design_matrix(frame_times, events, hrf_model='glover')
@@ -21,8 +23,23 @@ for vid, events in all_events.items():
     plot_design_matrix(X, ax=ax)
     ax.set_title(f'{vid[:-4]} design matrix', fontsize=12)
     plt.show()
-    plt.close()
+    break 
+    #plt.close()
 
 with open('./design_matrices_per_movie.pickle','wb') as f:
     pickle.dump(all_design_matrices, f)
 
+
+#Efficiency
+def efficiency(X):
+    '''Calculate efficiency for a given design matrix (i.e a given video) '''       
+    invXtX = np.linalg.inv(X.T.dot(X))
+    #Constrasts - need to verify
+    #Main(common) effect (1,1) vs differential effect (1, -1)
+    contrasts = np.ones((invXtX.shape[0], 1)) #Need to verify: np.ones((invXtX.shape))
+    efficiency = 1.0/(np.trace(contrasts.T.dot(invXtX).dot(contrasts)))
+    
+    return efficiency
+
+
+    
