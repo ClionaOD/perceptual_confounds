447e90bebc6c4ed2246110bb0d4738e0bdd5ca7f
hanmacrad2 <craddoch@tcd.ie>
2021-01-14 21:21:28 +0000

    leave one out efficiency calculation #2 - inv(X.T.dot(X)) - currently non invertible
    leave one out efficiency calculation #2 - inv(X.T.dot(X)) - currently non invertible

--- efficiency_calculation_lvo.py
+++ efficiency_calculation_lvo.py
@@ -9,13 +9,23 @@ import pandas as pd
 import numpy as np
 import random
 from nilearn.glm.first_level import make_first_level_design_matrix
-from nilearn.plotting import plot_design_matrix
-import matplotlib.pyplot as plt
+#from nilearn.plotting import plot_design_matrix
+#import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
 
-#Functions
+#Data
+with open('./events_per_movie.pickle','rb') as f:
+    all_events = pickle.load(f)
+    
+#Params
+tr = 1.0
+n_scans = 22
+frame_times = np.arange(n_scans) * tr
 
-def efficiency_calc(X, contrast_vec):
-    '''Calculate efficiency for a given design matrix (i.e a given video) '''       
+def efficiency_calc(X, contrasts):
+    '''Calculate efficiency for a given design matrix (i.e a given video) '''     
+    
+    #Singular matrix - no solution to inverse 
     invXtX = np.linalg.inv(X.T.dot(X))
     efficiency = np.trace((1.0/ contrasts.T.dot(invXtX).dot(contrasts)))
     
@@ -31,36 +41,44 @@ def get_rest_df(rest_length):
     
     return df_rest
 
+
 def get_df_events(all_events, rest_length = 2.0):
-    ''' '''
+    '''Get concatenated events dataframe. A period of rest in between each video is included'''
+    
     #List of videos + randomise
-    list_videos = all_events.keys()
+    list_videos = list(all_events.keys())
     random.shuffle(list_videos) #Randomise movie order
     
     #Params
     movie_length = 22.524
-    delay = 0.01
+    delay = 0.001
     df_rest = get_rest_df(rest_length)
     df_events_concat = pd.DataFrame()
     
-    for idx, vid in enumerate(list_videos):
-        
+    for idx, vid in enumerate(list_videos):      
         df_eventX = all_events[vid]
         df_temp = pd.concat([df_eventX, df_rest])
         #Adjust onsets of event
-        df_temp['onset'] = df_event['onset'] + idx*(movie_length + rest_length + delay)
+        df_temp['onset'] = df_temp['onset'] + idx*(movie_length + rest_length + delay)
         
         #Concatenate
         df_events_concat = pd.concat([df_events_concat,  df_temp])
+    
+    return df_events_concat
 
  
-def get_efficiencies_LVO(all_events_dict, required_video_num, contrast_vec):
+def get_efficiencies_LVO(all_events_dict, required_video_num, list_contrast_catgs):
+    
+    '''Using a leave one out principle to determine most 'efficient' videos.
+    Remove one video at a time and calculate the updated efficiency.
+    Repeat for each video. Drop the video that corresponds to the greatest efficiency when it is left out in the loo iteration.
+    Repeat until required number of videos remains'''
     
     #Repeat until required_video_num of videos remains 
-    while(all_events_dict < required_video_num):        
+    while(len(all_events_dict) > required_video_num):        
 
         #Dict to store efficiencies
-        dict_lvo_efficiencies = {vid: None, for vid in all_events_dict.keys()}
+        dict_lvo_efficiencies = {vid: None for vid in all_events_dict.keys()}
                
         for vid in all_events_dict.keys():
             
@@ -69,42 +87,48 @@ def get_efficiencies_LVO(all_events_dict, required_video_num, contrast_vec):
             
             #Get updated efficiencies - when leave one out 
             df_events = get_df_events(lvo_dict)
+            print(df_events.tail(30))
             X = make_first_level_design_matrix(frame_times, df_events, hrf_model='glover')
+            #Get contrasts
+            contrast_vec = create_contrast_vec(list_contrast_catgs, X)
             #Calculate efficiency 
             dict_lvo_efficiencies[vid] = efficiency_calc(X, contrast_vec)
             
         #Find video which corresponds to the highest efficiency when dropped
-        vid_max_improvement = max(dict_test, key=dict_test.get)
+        vid_max_improvement = max(dict_lvo_efficiencies, key=dict_lvo_efficiencies.get)
         #Drop from total events
         all_events_dict.pop(vid_max_improvement)
     
     return all_events_dict
 
+def create_contrast_vec(list_contrast_catgs, X):
+    'Create vector of contrasts'
+    
+    contrast_vec = np.zeros(X.shape[1]) #Set all to zero - default value unless categories present
+    
+    #Positive contrast (for example animate)
+    if list_contrast_catgs[0] in X.columns:
+
+        idx_pos = X.columns.get_loc(list_contrast_catgs[0])
+        contrast_vec[idx_pos] = 1 #Set contrast to 1 at position in vector corresponding to catgory in design matrix
+        
+        #Set remaining conditions to be negative contrast
+        for neg_catg in list_contrast_catgs[1:]:
+            if neg_catg in X.columns:
+                idx_pos = X.columns.get_loc(neg_catg) #Get column index of category
+                contrast_vec[idx_pos] = -1
+    
+    #print(contrast_vec)
+    return contrast_vec
+
 #Apply functions
 required_video_num = 8
-contrasts_vec = 
-events_top_movies = get_efficiencies_LVO(all_events_dict, required_video_num, contrast_vec)
-        
-        
-#Inspect all events
-#Find where animate is & inanimate is 
+list_contrast_catgs = ['animate', 'inanimate_small', 'inanimate_big']
+events_top_movies = get_efficiencies_LVO(all_events, required_video_num, list_contrast_catgs)
+ 
 
-def create_contrast_vec(list_contrasts, X):
-    'Create vector of contrasts. If value not present?'
-    
-    idx_contrasts = []
-    
-    for contrast in list_contrasts:
-        if contrast in df.columns:
-    
-    for idx, col in enumerate(df.columns):
-        if col in list_contrasts:
-            idx_contrasts.append(idx)
-            
-            
-        
-    
 
         
-    
+        
+
     
